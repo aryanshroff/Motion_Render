3c795bcea5d4287f048cc341f38ccc85396cd2b5
Aryan <aryanshroff21@gmail.com>
2023-06-15 18:14:55 +0530

    base_folder_generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-
+import { BrowserRouter } from "react-router-dom";
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components';
 
 const App = () => {
-  return <>3d developer Protfolio</>;
+  return (
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar></Navbar>
+        <Hero></Hero>
+      </div>
+      <About></About>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+    </BrowserRouter> 
+  );
 };
 
 export default App;
